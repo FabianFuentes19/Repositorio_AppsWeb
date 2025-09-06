a1fe4a6b00d7594fdcf3febdc6a124c790af58fe
FabianFuentes19 <20243ds032@utez.edu.mx>
2025-09-05 22:28:57 -0600

    en la clase depositar se implemento un metodo el cual nos ayudara a ejercutar un deposito, dentro de un try le pedira al usuario que ingrese la cantidad que quiere depositar, por medio de un if evaluara si la cantidad ingresada no es 0 o menor que 0, en caso de que asi sea mostrara un mensaje de errore. Caso contrario mostrara un mensaje de deposito exitoso. En caso de que el usuario no inngrese numeros e ingrese alguna letra o caracter, por medio  del catch hara la excepcion y mostrara asi un mensaje correspondiente, evitando que el programa termine su ejecucion repentinamente

--- Banco/src/Depositar.java
+++ Banco/src/Depositar.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
 public class Depositar {
+    private Scanner sc = new Scanner(System.in);
+
+    public void ejecutar(Cuenta cuenta) {
+        try {
+            System.out.print("Ingrese cantidad a depositar: ");
+            double cantidad = sc.nextDouble();
+
+            if (cantidad <= 0){
+                System.out.println(" no se permiten ingresar valores negativos o cero");
+                return;
+            }
+            cuenta.setSaldo(cuenta.getSaldo() + cantidad);
+            System.out.println("Deposito exitoso, saldo actual: $" + cuenta.getSaldo());
+        }catch (Exception e){
+            System.out.println("entrada invalida, solo se permiten numeros");
+            sc.nextLine();
+        }
+
+    }
 }
+
