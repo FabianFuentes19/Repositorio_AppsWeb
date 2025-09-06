37f0954b523953770831279d5cb3b2178526dcd9
FabianFuentes19 <20243ds032@utez.edu.mx>
2025-09-05 22:32:16 -0600

    se implemento en la clase retirar un metodo el cual ejecutara un retiro, primero muestra el saldo actual y despues pide al usuario que ingrese la cantidad que quiere retirar, si el usuario ingresa una cantidad mayor a su saldo actual, el retiro no podra realizarse, caso contrario mostrara un mensaje de deposito exitoso

--- Banco/src/Retirar.java
+++ Banco/src/Retirar.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Retirar {
+
+    private Scanner sc = new Scanner(System.in);
+
+    public void ejecutar(Cuenta cuenta) {
+        System.out.println("Saldo actual: $" + cuenta.getSaldo());
+        System.out.print("Ingrese cantidad a retirar: ");
+        double cantidad = sc.nextDouble();
+
+        if (cantidad > cuenta.getSaldo()) {
+            System.out.println("Fondos insuficientes.");
+        } else {
+            cuenta.setSaldo(cuenta.getSaldo() - cantidad);
+            System.out.println("Retiro exitoso. Nuevo saldo: $" + cuenta.getSaldo());
+        }
+    }
 }
