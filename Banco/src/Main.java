33fbd057ab7d858f3c656fce0c74791109bf79bf
FabianFuentes19 <20243ds032@utez.edu.mx>
2025-09-05 22:36:10 -0600

    en la clase main, se instancias para cada clase, ademas de darle un valor inicial al saldo. unicamente se le mostrara al usuario un menu con las diferentes acciones que puede realizar, una vez realizada la accion volvera al menu, el proceso termina cuando el usuario ingresa la opcion 4 la cual es salir

--- Banco/src/Main.java
+++ Banco/src/Main.java
@@ -1,15 +1,44 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sc = new Scanner(System.in);
+        Cuenta cuenta = new Cuenta(0.0);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Consultar consu = new Consultar();
+        Depositar deposito = new Depositar();
+        Retirar reti = new Retirar();
+
+        int opcion;
+        do {
+            System.out.println("\n=== Menu Banco ===");
+            System.out.println("1. Consultar saldo");
+            System.out.println("2. Depositar");
+            System.out.println("3. Retirar");
+            System.out.println("4. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    consu.mostrarSaldo(cuenta);
+                    break;
+                case 2:
+                    deposito.ejecutar(cuenta);
+                    break;
+                case 3:
+                    reti.ejecutar(cuenta);
+                    break;
+                case 4:
+                    System.out.println("Gracias por usar el banco.");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        } while (opcion != 4);
+
+        sc.close();
     }
 }
